4b8a6864531461770357cfd230160a76978243f4
MetinovAdik <metinovadilet@gmail.com>
2024-02-25 02:42:18 +0600

    changed whisper commands

--- src/main/java/com/techdragons/transcibe/TranscriptionController.java
+++ src/main/java/com/techdragons/transcibe/TranscriptionController.java
@@ -1,19 +1,19 @@
 package com.techdragons.transcibe;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 public class TranscriptionController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TranscriptionController.class);
     @Autowired
     private FileDownloadService fileDownloadService;
 
@@ -25,18 +25,20 @@ public class TranscriptionController {
 
     @PostMapping("/transcribe")
     public ResponseEntity<String> transcribe(@RequestBody TranscriptionRequest request) {
-        System.out.println(request.getPassword());
-        System.out.println(servicePassword);
+        logger.info("Получен запрос на транскрипцию для URL: {}", request.getMediaUrl());
         if (!servicePassword.equals(request.getPassword())) {
+            logger.warn("Неавторизованный доступ с паролем: {}", request.getPassword());
             return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
         }
         try {
             Path downloadedFile = fileDownloadService.downloadMedia(request.getMediaUrl(), request.getMediaType());
+            logger.info("Файл успешно скачан и сохранен как: {}", downloadedFile.toString());
             String transcription = transcriptionService.recognizeSpeechFromMedia(downloadedFile.toString());
+            logger.info("Транскрипция завершена");
             Files.delete(downloadedFile);
             return new ResponseEntity<>(transcription, HttpStatus.OK);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Ошибка при обработке медиафайла", e);
             return new ResponseEntity<>("Error processing the media file", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
