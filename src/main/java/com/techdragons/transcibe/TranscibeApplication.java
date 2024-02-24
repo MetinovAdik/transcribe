f7d52b9365ced51185564d6fba9339be9bcccb00
MetinovAdik <metinovadilet@gmail.com>
2024-02-24 23:04:22 +0600

    docker details

---
+++ src/main/java/com/techdragons/transcibe/TranscibeApplication.java
@@ -0,0 +1,13 @@
+package com.techdragons.transcibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techdragons/transcibe/TranscibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdragons.transcibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
