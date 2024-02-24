f44c3bc5aa65bb09d10f8094f7a444019644cc19
MetinovAdik <metinovadilet@gmail.com>
2024-02-25 02:46:03 +0600

    changed whisper commands

--- src/main/java/com/techdragons/transcibe/TranscriptionController.java
+++ src/main/java/com/techdragons/transcibe/TranscriptionController.java
@@ -8,12 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 @RestController
 public class TranscriptionController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TranscriptionController.class);
     @Autowired
     private FileDownloadService fileDownloadService;
 
@@ -25,20 +22,15 @@ public class TranscriptionController {
 
     @PostMapping("/transcribe")
     public ResponseEntity<String> transcribe(@RequestBody TranscriptionRequest request) {
-        logger.info("Получен запрос на транскрипцию для URL: {}", request.getMediaUrl());
         if (!servicePassword.equals(request.getPassword())) {
-            logger.warn("Неавторизованный доступ с паролем: {}", request.getPassword());
             return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
         }
         try {
             Path downloadedFile = fileDownloadService.downloadMedia(request.getMediaUrl(), request.getMediaType());
-            logger.info("Файл успешно скачан и сохранен как: {}", downloadedFile.toString());
             String transcription = transcriptionService.recognizeSpeechFromMedia(downloadedFile.toString());
-            logger.info("Транскрипция завершена");
             Files.delete(downloadedFile);
             return new ResponseEntity<>(transcription, HttpStatus.OK);
         } catch (Exception e) {
-            logger.error("Ошибка при обработке медиафайла", e);
             return new ResponseEntity<>("Error processing the media file", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
